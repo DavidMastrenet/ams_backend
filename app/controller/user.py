c3aa4387f0b3800453f384279877a2a7feb7eba2
洪宇轩 <hongyuxuan@xysu.tech>
2023-12-31 09:19:25 +0800

    remove. 测试数据

--- app/controller/user.py
+++ app/controller/user.py
@@ -1,10 +1,9 @@
-from flask import request, redirect, jsonify
+from flask import request, redirect
 from flask_login import login_required, logout_user
 
 from app.controller import user_bp
 from app.auth.login import Login
 
-from app.dao.user import UserManager
 
 @user_bp.route('/api/login', methods=['POST'])
 def login():
@@ -19,8 +18,3 @@ def login():
 def logout():
     logout_user()
     return redirect('/')
-
-@user_bp.route('/api/role', methods=['GET'])
-def get_role():
-    user_manager = UserManager("1000555508")
-    return user_manager.get_user_role()
